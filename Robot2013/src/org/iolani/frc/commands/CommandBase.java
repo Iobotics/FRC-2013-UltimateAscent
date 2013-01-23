e6576e7f0d6faed6a2d275b9fc9b568c18185b2a
Jeff Malins <jmalins@iolani.org>
2013-01-22 15:48:51 -1000

    added first subsystem class

--- Robot2013/src/org/iolani/frc/commands/CommandBase.java
+++ Robot2013/src/org/iolani/frc/commands/CommandBase.java
@@ -2,8 +2,8 @@ package org.iolani.frc.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.iolani.frc.OI;
-import org.iolani.frc.subsystems.ExampleSubsystem;
+import org.iolani.frc.*;
+import org.iolani.frc.subsystems.*;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
@@ -15,7 +15,7 @@ public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
-    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static Drivetrain drive = new Drivetrain();
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
@@ -26,7 +26,7 @@ public abstract class CommandBase extends Command {
         oi = new OI();
 
         // Show what command your subsystem is running on the SmartDashboard
-        SmartDashboard.putData(exampleSubsystem);
+        SmartDashboard.putData(drive);
     }
 
     public CommandBase(String name) {
