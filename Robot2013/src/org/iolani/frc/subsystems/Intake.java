9f049dd9aa8875b6e198f125a47b7d1860954d78
Rachael Ridao <raridao@iolani.org>
2013-01-25 16:57:15 -1000

    Finished skeleton of Intake class

--- Robot2013/src/org/iolani/frc/subsystems/Intake.java
+++ Robot2013/src/org/iolani/frc/subsystems/Intake.java
@@ -15,14 +15,56 @@ import org.iolani.frc.util.*;
  */
 public class Intake extends Subsystem {
     private static Victor _victor;
+    private double _power;
+    private IntakeMode _mode;
     
     public void init() {
         _victor = new Victor(RobotMap.intakePWM);
-        _intake = new 
     }
     
-    public void rotate(double speed){
-        _victo
+    public void setIntakePower(double pwr) {
+        _power = pwr;
+        _victor.set(pwr);
+    }
+    
+    public double getPower() {
+        return _power;
+    }
+    
+    public void setIntake(IntakeMode mode) {
+        if(_mode == mode) return; 
+        switch(mode.value) {
+            case IntakeMode.kOff_val:
+                _victor.set(0.0);
+                break;
+            case IntakeMode.kSuck_val:
+                _victor.set(-1.0);
+                break;
+            case IntakeMode.kBlow_val:
+                _victor.set(1.0);
+                break;
+        }
+        _mode = mode;
+    }
+    
+    public IntakeMode getIntake() {
+        return _mode;
+    }
+    
+    public static final class IntakeMode {
+        public static final int kOff_val  = 0;
+        public static final int kSuck_val = 1;
+        public static final int kBlow_val = 2;
+        
+        public static final IntakeMode kOff  = new IntakeMode(kOff_val);
+        public static final IntakeMode kSuck = new IntakeMode(kSuck_val);
+        public static final IntakeMode kBlow = new IntakeMode(kBlow_val);
+        
+        private int value;
+        
+        private IntakeMode (int val) {
+            value = val;
+        }
     }
     
     public void initDefaultCommand() {
