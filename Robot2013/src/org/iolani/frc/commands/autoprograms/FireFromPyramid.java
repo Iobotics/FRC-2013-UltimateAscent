82c00c9a8e448f227841c4a49f79e0d80d443aa6
Jeff Malins <jmalins@iolani.org>
2013-04-04 22:18:11 -1000

    Added elevation control

--- Robot2013/src/org/iolani/frc/commands/autoprograms/FireFromPyramid.java
+++ Robot2013/src/org/iolani/frc/commands/autoprograms/FireFromPyramid.java
@@ -7,6 +7,7 @@ package org.iolani.frc.commands.autoprograms;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.iolani.frc.commands.FireFrisbee;
+import org.iolani.frc.commands.SetShooterElevation;
 import org.iolani.frc.commands.SetShooterPower;
 
 /**
@@ -17,9 +18,10 @@ public class FireFromPyramid extends CommandGroup {
     
     public FireFromPyramid() {
         this.addSequential(new SetShooterPower(1.0));
+        this.addSequential(new SetShooterElevation(true));
         this.addSequential(new WaitCommand(3.0));
         this.addSequential(new FireFrisbee());
         this.addSequential(new FireFrisbee());
-        this.addSequential(new SetShooterPower(1.0));
+        this.addSequential(new SetShooterPower(0.0));
     }
 }
